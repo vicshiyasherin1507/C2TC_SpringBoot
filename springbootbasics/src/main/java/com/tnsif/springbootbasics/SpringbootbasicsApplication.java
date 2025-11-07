4e88fc176e5f36f756ad1adc55c62db038376754
WELCOME <WELCOME@DESKTOP-L7JP3CA>
2025-11-07 12:11:36 +0530

    springbootbasics

---
+++ springbootbasics/src/main/java/com/tnsif/springbootbasics/SpringbootbasicsApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.springbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicsApplication.class, args);
+	}
+
+}
